fe886ba676349dc099ba8276a2f097ccafb6a2a5
YK423a <mwdjg0@gmail.com>
2025-07-13 18:10:44 +0900

    Add files via upload

--- kadai_018/KatoExec_Chapter18.java
+++ kadai_018/KatoExec_Chapter18.java
@@ -6,8 +6,13 @@ public class KatoExec_Chapter18 {
 		Kato_Chapter18 ichiro = new KatoIchiro_Chapter18();
 		Kato_Chapter18 hanako = new KatoHanako_Chapter18();
 		
+		taro.setGivenName("太郎");
 		taro.execIntroduce();
+		
+		ichiro.setGivenName("一郎");
 		ichiro.execIntroduce();
+		
+		hanako.setGivenName("花子");
 		hanako.execIntroduce();
 	}
 }
